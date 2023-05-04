9698071acf07bb034f02b4d17c0739b33a857310
SaraShoukry <s.yahia93@gmail.com>
2023-05-05 00:49:32 +0200

    Init project, Create migrations & add jwt

---
+++ database/migrations/2023_05_04_212556_create_borrows_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBorrowsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('borrows', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('book_id');
+            $table->unsignedBigInteger('customer_id');
+
+            $table->date('start_date');
+            $table->date('end_date');
+
+            $table->foreign('book_id')->references('id')->on('books');
+            $table->foreign('customer_id')->references('id')->on('customers');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('borrows');
+    }
+}
